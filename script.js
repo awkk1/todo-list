a9de85eeabf82b7c243aedebce710b7b5b497b5e
Awkk1 <awkki397@gmail.com>
2026-04-25 17:42:51 +0300

    feat: add edit button with initial UI styling

--- script.js
+++ script.js
@@ -83,8 +83,20 @@ function renderTasks() {
             }
             li.appendChild(p);
 
+            let buttonForEdit = document.createElement("button");
+            buttonForEdit.classList.add("button-for-edit");
+            buttonForEdit.dataset.action = "edit";
+            const editIcon = `
+            <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M11 4H6C4.93913 4 3.92178 4.42142 3.17163 5.17157C2.42149 5.92172 2 6.93913 2 8V18C2 19.0609 2.42149 20.0783 3.17163 20.8284C3.92178 21.5786 4.93913 22 6 22H17C19.21 22 20 20.2 20 18V13" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>`
+            buttonForEdit.innerHTML = editIcon;
+            li.appendChild(buttonForEdit);
+
             let buttonForDelete = document.createElement("button");
             buttonForDelete.classList.add("delete-task-button");
+            buttonForDelete.dataset.action = "delete";
 
             const trashIcon = `
         <svg class="delete-task-icon" width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -103,6 +115,10 @@ list.addEventListener("dblclick", function (e) {
     if (e.target.closest("button") || e.target.type === "checkbox") return;
     if (list.querySelector(".edit-input")) return;
 
+    startEdit(li);
+});
+
+function startEdit(li) {
     const li_index = Number(li.dataset.index);
     let inputNew= document.createElement("input");
     let p = li.querySelector("p");
@@ -113,7 +129,7 @@ list.addEventListener("dblclick", function (e) {
     inputNew.value = p_content;
     inputNew.focus();
     inputNew.select();
-    
+
     let isSaved = false;
 
     function saveEdit() {
@@ -155,10 +171,10 @@ list.addEventListener("dblclick", function (e) {
         if (isSaved) return;
         saveEdit();
     })
-});
+}
 
 list.addEventListener("click", function (e) {
-    let li = e.target.closest("li");
+    let li = e.target.closest(".task");
     if (!li) {
         return
     }
@@ -167,7 +183,7 @@ list.addEventListener("click", function (e) {
     }
     let index = Number(li.dataset.index);
 
-    if (e.target.closest("button")) {
+    if (e.target.closest('[data-action="delete"]')) {
         li.classList.add("task-for-delete");
         setTimeout(() => {
             tasks.splice(index, 1);
@@ -178,6 +194,8 @@ list.addEventListener("click", function (e) {
         },
             505);
         return;
+    } else if (e.target.closest('[data-action="edit"]')) {
+        startEdit(li);
     };
 
     if (e.target.type === "checkbox") {
